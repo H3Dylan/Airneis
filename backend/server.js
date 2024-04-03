0a812e4e2f1c89de7c16f3b9d0aa7331e4cd182a
dasilv-h3 <k.dasilva@h3hitema.fr>
2024-04-03 21:52:40 +0200

    connection db to node server

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const cors = require('cors');
-
+const connectDB = require('./database/db');
 const app = express();
 const PORT = 5000;
 
 app.use(cors())
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-});
-
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+    connectDB();
 });
