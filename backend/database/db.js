c1e312a9cee2e8ba0e1546ebee62437aa38bc04a
dasilv-h3 <k.dasilva@h3hitema.fr>
2024-04-05 13:52:56 +0200

    changement port NodeJS et url db

--- backend/database/db.js
+++ backend/database/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://db:27017/airneisDB');
+        await mongoose.connect('mongodb://localhost:27017/airneisDB');
         console.log('Connected to MongoDB');
     } catch (err) {
         console.error('Error connecting to MongoDB:', err);
