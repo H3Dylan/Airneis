bcc0162ebb474daff034d526ac13944356d11f6f
H3Dylan <d.bouillon@h3hitema.fr>
2024-07-29 01:32:24 +0200

    test

--- backoffice/src/App.jsx
+++ backoffice/src/App.jsx
@@ -1,7 +1,7 @@
-// src/App.js
+// src/App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import ArticleList from './components/ArticleList';
+import ArticleList from './components/ArticleList.jsx';
 
 const App = () => {
     return (
